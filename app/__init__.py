87b444d630dc39ba0ff9b4544c93440d62db3fb3
Jimmy Shong <jimmysh341@gmail.com>
2024-05-12 18:36:10 -0400

    Syncing with database

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import Flow  # type: ignore
 from googleapiclient.discovery import build
 from werkzeug.wrappers import Response
-
+from .database import * 
 
 load_dotenv()
 
@@ -57,8 +57,9 @@ flow = Flow.from_client_config(
 @app.route("/add_task", methods=["POST"])
 def add_task() -> Union[wrappers.Response, tuple[wrappers.Response, int]]:
     try:
+        mydb = get_database()
         task_title = request.form.get("title")
-        task_details = request.form.get("details")
+        task_notes = request.form.get("notes")
         task_due = request.form.get("due")
 
         if not task_title:
@@ -70,21 +71,27 @@ def add_task() -> Union[wrappers.Response, tuple[wrappers.Response, int]]:
         credentials = Credentials.from_authorized_user_info(json.loads(session["credentials"]))
 
         service = build("tasks", "v1", credentials=credentials)
+        userinfo_service = build("oauth2", "v2", credentials=credentials)
+        user_info = userinfo_service.userinfo().get().execute()
+        your_email = user_info.get("email")
+        
         if task_due:
             task_due = task_due + "T23:59"
             date = datetime.strptime(task_due, "%Y-%m-%dT%H:%M").isoformat() + "Z"
             task = (
-                {"title": task_title, "notes": task_details, "due": date}
-                if task_details
-                else {"title": task_title, "notes": None, "due": date}  # type: ignore
+                {"title": task_title, "notes": task_notes, "due": date}
+                if task_notes
+                else {"title": task_title, "notes": "None", "due": date}  # type: ignore
             )
         else:
             task = (
-                {"title": task_title, "notes": task_details, "due": None}  # type: ignore
-                if task_details
+                {"title": task_title, "notes": task_notes, "due": None}  # type: ignore
+                if task_notes
                 else {"title": task_title, "notes": None, "due": None}  # type: ignore
             )
         result = service.tasks().insert(tasklist="@default", body=task).execute()  # type: ignore
+        add_task_db(mydb, result["id"], result["title"], task["notes"], task["due"], your_email)
+        print(result)
         return jsonify({"success": True, "task": result})
     except Exception as e:
         logging.error("Error adding task: %s", e)
@@ -94,29 +101,24 @@ def add_task() -> Union[wrappers.Response, tuple[wrappers.Response, int]]:
 @app.route("/list_tasks")
 def list_tasks() -> Union[wrappers.Response, tuple[wrappers.Response, int]]:
     try:
+        mydb = get_database()
         if "credentials" not in session:
             return jsonify({"error": "User credentials not found"}), 401
 
         credentials = Credentials.from_authorized_user_info(json.loads(session["credentials"]))
 
         service = build("tasks", "v1", credentials=credentials)
+        userinfo_service = build("oauth2", "v2", credentials=credentials)
+        user_info = userinfo_service.userinfo().get().execute()
+        your_email = user_info.get("email")
 
         # Example: Retrieve tasks from Google Tasks API
         tasks_result = service.tasks().list(tasklist="@default").execute()
-        tasks = tasks_result.get("items", [])  # Extract tasks from response
+        tasks = tasks_result.get("items", []) 
+        update_task_db(mydb, tasks, your_email)
 
-        # Format tasks into a list of dictionaries
-        formatted_tasks = [
-            {"id": task["id"], "title": task["title"], "details": task.get("notes"), "due": task["due"]}
-            if task.get("due") and task.get("notes")
-            else {"id": task["id"], "title": task["title"], "details": task.get("notes")}
-            if task.get("notes")
-            else {"id": task["id"], "title": task["title"], "due": task.get("due")}
-            if task.get("due")
-            else {"id": task["id"], "title": task["title"]}
-            for task in tasks
-        ]
-        return jsonify(formatted_tasks)
+        # Extract tasks from response
+        return jsonify(tasks)
     except Exception as e:
         logging.error("Error listing tasks: %s", e)
         return jsonify({"error": "Failed to list tasks"}), 500
@@ -125,6 +127,7 @@ def list_tasks() -> Union[wrappers.Response, tuple[wrappers.Response, int]]:
 @app.route("/delete_task", methods=["POST"])
 def delete_task() -> Union[wrappers.Response, tuple[wrappers.Response, int]]:
     try:
+        mydb = get_database()
         task_id = request.form.get("id")
         if not task_id:
             return jsonify({"error": "Task ID is required"}), 400
@@ -135,8 +138,11 @@ def delete_task() -> Union[wrappers.Response, tuple[wrappers.Response, int]]:
         credentials = Credentials.from_authorized_user_info(json.loads(session["credentials"]))
 
         service = build("tasks", "v1", credentials=credentials)
-
+        userinfo_service = build("oauth2", "v2", credentials=credentials)
+        user_info = userinfo_service.userinfo().get().execute()
+        your_email = user_info.get("email")
         service.tasks().delete(tasklist="@default", task=task_id).execute()
+        delete_task_db(mydb, task_id, your_email)
         return jsonify({"success": True})
     except Exception as e:
         logging.error("Error deleting task: %s", e)
@@ -236,6 +242,7 @@ def list_comments() -> Union[wrappers.Response, tuple[wrappers.Response, int]]:
                 relevant_comments = [
                     {
                         "id": comment["id"],
+                        "user": your_email,
                         "author": comment["author"]["displayName"],  # type: ignore
                         "filtered_content": comment["content"].replace("@" + your_email, ""),
                         "file": file["name"],
--- app/database.py
+++ app/database.py
@@ -1,42 +1,62 @@
 from pymongo import MongoClient
 
 
+
 # Define a function to get the database and tasks collection
 def get_database():
-
+	global db_client, mydb, tasks, comments, db_name
 	# Connect to MongoDB server running on localhost at default port 27017
 	db_client = MongoClient('localhost', 27017)
+	db_name = "mydatabase"
 
 	# Access or create a database named "mydatabase"
-	mydb = db_client["mydatabase"]
+	mydb = db_client[db_name]
 
 	# Access or create a collection named "tasks" within the "mydatabase" database
 	tasks = mydb["tasks"]
 
 	# Return references to the database and the tasks collection
-	return mydb,tasks
+	return mydb
 
 # Define a function to add a task to the tasks collection in the database
-def add_task_db(tasks,task_id,task_name, task_details,user_id):
-	task_dict = {"_id":task_id, "title": task_name, "description": task_details, "user":user_id}
+def add_task_db(mydb, task_id,task_name, task_details, due, user_id):
+	task_dict = {"_id":task_id, "title": task_name, "notes": task_details, "due": due, "user": user_id}
 	if len(list(tasks.find(task_dict))) == 0:
-		tasks.insert_one(task_dict)
-
+		mydb.tasks.insert_one(task_dict)
 	else:
 		print("task already exists")
 
 # Define a function to delete a task from the tasks collection in the database
-def delete_task_db(tasks, task_id, user_id):
-	task_dict = {"_id":task_id, "user":user_id}
-	tasks.delete_one(task_dict)
+def delete_task_db(mydb, task_id, user_id):
+	task_dict = {"_id":task_id, "user": user_id}
+	if len(list(tasks.find(task_dict))) > 0:
+		mydb.tasks.delete_one(task_dict)
+	else:
+		print("task does not exists")
+
+#Define a function to update tasks associated with a specific user from the tasks collection in the database
+def update_task_db(mydb, tasks, user_id):
+    for task in tasks:
+        print(task)
+        task_dict = {"_id": task["id"], "user": user_id}
+        update_data = {"$set": {
+            "title": task["title"], 
+            "notes": task.get("notes"),  # Use empty string as default if 'notes' is not provided
+            "due": task.get("due")  # Use None as default if 'due' is not provided
+        }}
+        
+        # The update_one method will update the document if it exists, or insert it if it does not (upsert=True)
+        result = mydb.tasks.update_one(task_dict, update_data, upsert=True)
+        
+        # You can print the result of the update to check how many documents were matched and modified
+        print(f"Documents matched: {result.matched_count}, Documents modified: {result.modified_count}")
 
 #Define a function to fetch tasks associated with a specific user from the tasks collection in the database
-def fetch_task_db(tasks, user_id):
+def fetch_task_db(mydb, user_id):
 	task_dict = {"user":user_id}
-	task_list = tasks.find(task_dict)
-
-	return list(task_list)
-
+	task_list = mydb.tasks.find(task_dict)
+	result = list(task_list)
+	return result[::-1]
 
 
 
